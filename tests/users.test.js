e43df205b39214ca14d61269570b0d52b196d350
Richard Fabros <Richard_Clarence_Dayo_Fabros@manulife.com>
2021-05-02 22:16:24 +0800

    some tests and helmet

--- tests/users.test.js
+++ tests/users.test.js
@@ -12,8 +12,16 @@ test("Invalid email is rejected", () => {
     createUser("John", "Doe", "johndoesfakeemail", {
       userRepository: new UserRepository(),
     });
-  }).toThrow("User failed email validation");
+  }).to
 });
+
+// test("Valid email is accepted", () => {
+//   expect(
+//     createUser("John", "Doe", "johndoes@email.co", {
+//       userRepository: new UserRepository(),
+//     })).not.toBeNull();
+// });
+
 
 test("User Repository Persist is called once", () => {
   const mockedRepo = new UserRepository();
@@ -23,3 +31,4 @@ test("User Repository Persist is called once", () => {
 
   expect(mockedRepo.persist.mock.calls.length).toBe(1);
 });
+
